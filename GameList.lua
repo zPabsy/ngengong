68cd58b3ad3f6350cfb3ed35aa085b926dd38e72
Hendrik <zpabsy@gmail.com>
2025-08-01 16:33:36 +0700

    Create GameList.lua

---
+++ GameList.lua
@@ -0,0 +1,6 @@
+local Games = {
+  [126884695634066] = "https://raw.githubusercontent.com/", -- Grow a Garden
+  [91867617264223] = "https://raw.githubusercontent.com/", -- Grow a Garden 1
+}
+
+return Games
